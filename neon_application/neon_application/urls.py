b6b905f274ee48d14805c2dad4c7da1b55f4a326
namanchoudhary28 <65090808+namanchoudhary28@users.noreply.github.com>
2020-05-13 04:26:57 +0530

    Add files via upload

--- neon_application/neon_application/urls.py
+++ neon_application/neon_application/urls.py
@@ -1,4 +1,4 @@
-"""neon_application URL Configuration
+"""neon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from neon import views
+from neonapp import views
+
 
 urlpatterns = [
-    
-    path('',views.loginpage,name="loginpage"),
-   
-    path('profile/',include('neon.urls')),
     path('admin/', admin.site.urls),
+    
+
+    path('experience_form/',views.experience_form),
 ]
