c11a5aef8c7e1b95ed30b7604832790fb2cfaf98
sanskar jain <sanskarj@iitbhilai.ac.in>
2020-05-12 23:56:34 +0530

    the app is now connected with MySQL database. Four basic tables are created

--- neon_application/neon/models.py
+++ neon_application/neon/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=1000)
+    email = models.EmailField()
+    phonenumber = models.BigIntegerField()
+    password  = models.CharField(max_length=32)
+
+
+
+class skills(models.Model):
+    skill = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+class hobbies(models.Model):
+    hobby = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+class projects(models.Model):
+    project = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    
+    
+    
+    
+
--- neon_application/neon_application/settings.py
+++ neon_application/neon_application/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'neon_application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'students',
+        'USER': 'root',
+        'PASSWORD': 'jain@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
